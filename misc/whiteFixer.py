fa8277789852228e49d32f9f4f99e9141ffbe7d6
Octomagon <tinymousefarts@gmail.com>
2016-11-26 08:59:09 -0500

    Self-contained macReport.py

--- misc/whiteFixer.py
+++ misc/whiteFixer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import magic
-import os
+import os, pprint
 
 wl = [line.rstrip('\n') for line in open('./wl.txt')]
+
+pprint.pprint(wl)
+exit()
 
 for l in wl:
 	if os.path.exists(l):
